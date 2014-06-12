0aeb5898ff4cee72bd311e0d351669dcfd041558
Brian14 <brianbao@126.com>
2014-06-12 23:42:38 +0800

    decimal

--- src/main/java/com/corejava/number/TestDecimal.java
+++ src/main/java/com/corejava/number/TestDecimal.java
@@ -1,6 +1,7 @@
 package com.corejava.number;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 
 /**
  * User: bao
@@ -9,6 +10,15 @@ import java.math.BigDecimal;
 public class TestDecimal {
 
     public static void main(String args[]){
-        BigDecimal.
+        BigDecimal b= new BigDecimal(100.224);
+        DecimalFormat format = new DecimalFormat(".00");
+        String s = format.format(b);
+        System.out.println(s);
+
+        BigDecimal b2= new BigDecimal(100);
+        DecimalFormat format2 = new DecimalFormat(".00");
+        String s2 = format.format(b2);
+        System.out.println(s2);
+
     }
 }
