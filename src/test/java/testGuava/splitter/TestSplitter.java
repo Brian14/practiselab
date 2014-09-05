825aad1a51aedd01f2a6f35545803f6ccf021a21
Brian Bao <brian.bao@oocl.com>
2014-09-05 17:31:38 +0800

    123

--- src/test/java/testGuava/splitter/TestSplitter.java
+++ src/test/java/testGuava/splitter/TestSplitter.java
@@ -9,9 +9,13 @@ import org.junit.Test;
  */
 public class TestSplitter {
     @Test
-    public void testSplit(){
-//        Splitter.
+    public void testSplit() {
 
+        String s = "1;2;2;3;3;;4;5;     ;ss;";
+        Splitter blNoSplitter = Splitter.on(";").trimResults().omitEmptyStrings();
+
+        Iterable<String> iterable = blNoSplitter.split(s);
+        System.out.println(iterable);
 
     }
 
